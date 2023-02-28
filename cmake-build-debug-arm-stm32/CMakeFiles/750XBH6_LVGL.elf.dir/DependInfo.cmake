f39ca90360960b54378e8bcf8d095caeaf8f0aa1
fumoumou <2371714658@qq.com>
2023-02-28 15:39:55 +0800

    创建模板添加调试串口

---
+++ cmake-build-debug-arm-stm32/CMakeFiles/750XBH6_LVGL.elf.dir/DependInfo.cmake
@@ -0,0 +1,68 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Core/Startup/startup_stm32h750xbhx.s" "C:/Users/fumoumou/Desktop/750XBH6_LVGL/cmake-build-debug-arm-stm32/CMakeFiles/750XBH6_LVGL.elf.dir/Core/Startup/startup_stm32h750xbhx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32H750xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Core/Inc"
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Drivers/STM32H7xx_HAL_Driver/Inc"
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Drivers/STM32H7xx_HAL_Driver/Inc/Legacy"
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Drivers/CMSIS/Device/ST/STM32H7xx/Include"
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Core/Src/gpio.c" "CMakeFiles/750XBH6_LVGL.elf.dir/Core/Src/gpio.c.obj" "gcc" "CMakeFiles/750XBH6_LVGL.elf.dir/Core/Src/gpio.c.obj.d"
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Core/Src/main.c" "CMakeFiles/750XBH6_LVGL.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/750XBH6_LVGL.elf.dir/Core/Src/main.c.obj.d"
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Core/Src/stm32h7xx_hal_msp.c" "CMakeFiles/750XBH6_LVGL.elf.dir/Core/Src/stm32h7xx_hal_msp.c.obj" "gcc" "CMakeFiles/750XBH6_LVGL.elf.dir/Core/Src/stm32h7xx_hal_msp.c.obj.d"
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Core/Src/stm32h7xx_it.c" "CMakeFiles/750XBH6_LVGL.elf.dir/Core/Src/stm32h7xx_it.c.obj" "gcc" "CMakeFiles/750XBH6_LVGL.elf.dir/Core/Src/stm32h7xx_it.c.obj.d"
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Core/Src/syscalls.c" "CMakeFiles/750XBH6_LVGL.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/750XBH6_LVGL.elf.dir/Core/Src/syscalls.c.obj.d"
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Core/Src/sysmem.c" "CMakeFiles/750XBH6_LVGL.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/750XBH6_LVGL.elf.dir/Core/Src/sysmem.c.obj.d"
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Core/Src/system_stm32h7xx.c" "CMakeFiles/750XBH6_LVGL.elf.dir/Core/Src/system_stm32h7xx.c.obj" "gcc" "CMakeFiles/750XBH6_LVGL.elf.dir/Core/Src/system_stm32h7xx.c.obj.d"
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Core/Src/usart.c" "CMakeFiles/750XBH6_LVGL.elf.dir/Core/Src/usart.c.obj" "gcc" "CMakeFiles/750XBH6_LVGL.elf.dir/Core/Src/usart.c.obj.d"
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal.c" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal.c.obj" "gcc" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal.c.obj.d"
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_cortex.c" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_cortex.c.obj" "gcc" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_cortex.c.obj.d"
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma.c" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma.c.obj" "gcc" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma.c.obj.d"
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma_ex.c" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma_ex.c.obj.d"
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_exti.c" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_exti.c.obj" "gcc" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_exti.c.obj.d"
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash.c" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash.c.obj" "gcc" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash.c.obj.d"
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash_ex.c" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash_ex.c.obj.d"
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_gpio.c" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_gpio.c.obj" "gcc" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_gpio.c.obj.d"
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_hsem.c" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_hsem.c.obj" "gcc" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_hsem.c.obj.d"
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c.c" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c.c.obj" "gcc" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c.c.obj.d"
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c_ex.c" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c_ex.c.obj" "gcc" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c_ex.c.obj.d"
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_mdma.c" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_mdma.c.obj" "gcc" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_mdma.c.obj.d"
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr.c" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr.c.obj" "gcc" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr.c.obj.d"
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr_ex.c" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr_ex.c.obj.d"
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc.c" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc.c.obj" "gcc" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc.c.obj.d"
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc_ex.c" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc_ex.c.obj.d"
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim.c" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim.c.obj" "gcc" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim.c.obj.d"
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim_ex.c" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim_ex.c.obj.d"
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart.c" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart.c.obj" "gcc" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart.c.obj.d"
+  "C:/Users/fumoumou/Desktop/750XBH6_LVGL/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart_ex.c" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart_ex.c.obj" "gcc" "CMakeFiles/750XBH6_LVGL.elf.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart_ex.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
